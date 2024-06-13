b032901de896eecbfdc9967f262d7cba6f78783d
Tamerlan Eloev <tamerlan.eloeff2017@yandex.ru>
2024-06-13 21:20:21 +0300

    added mode to module.exports

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,6 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
-  devServer: {
-    hot: false,
-    devMiddleware: {
-      stats: false,
-    },
-  },
+  mode: "production"
 })
 
